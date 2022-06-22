9e8849a9443c557ecdc2198b6ff31992bf39b10b
Alim-ziedinov <a-zidan@rambler.ru>
2022-06-22 11:25:23 +0300

    fixed_test

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -2,15 +2,22 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static java.awt.SystemColor.text;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
 
 public class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
     public static void setUpAll() {
-        System.setProperty("Webdriver.geckodriver.driver", "./win/geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "./win/geckodriver.exe");
     }
 
     @BeforeEach
@@ -18,15 +25,26 @@ public class CallbackTest {
         driver = new FirefoxDriver();
     }
 
+
+    @Test
+    void shouldSubmitRequest() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Сюзанна");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79788586822");
+     //   List<WebElement> elements = driver.findElements(By.className("input__control"));
+     //   elements.get(0).sendKeys("Василий");
+      //  elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, text);
+
+    }
+
     @AfterEach
     public void tearDown() {
         driver.quit();
         driver = null;
-    }
-
-
-    @Test
-    voidshouldTestSomething() {
-        driver.get("ja");
     }
 }
