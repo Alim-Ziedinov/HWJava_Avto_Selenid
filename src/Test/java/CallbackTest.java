e4bea2c1372ecc57c88109c5dd39c2960d2df5cd
Alim-ziedinov <a-zidan@rambler.ru>
2022-06-23 17:46:09 +0300

    added_files

--- src/Test/java/CallbackTest.java
+++ src/Test/java/CallbackTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +18,8 @@ public class CallbackTest {
 
     @BeforeAll
     public static void setUpAll() {
-        System.setProperty("webdriver.gecko.driver", "./drivers/win/geckodriver.exe");
+      //  System.setProperty("webdriver.gecko.driver", "./drivers/linux/geckodriver");
+        WebDriverManager.firefoxdriver().setup();
     }
 
     @BeforeEach
