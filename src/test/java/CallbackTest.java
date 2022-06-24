768d2922afd123c1b51ee1091a16a7906a70c1a8
Alim-ziedinov <a-zidan@rambler.ru>
2022-06-24 12:53:16 +0300

    modified_Test&Appveyor

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -4,30 +4,28 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static java.awt.SystemColor.text;
 import static org.junit.jupiter.api.Assertions.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
-import java.util.List;
 
 public class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
     public static void setUpAll() {
-<<<<<<< HEAD:src/Test/java/CallbackTest.java
-      //  System.setProperty("webdriver.gecko.driver", "./drivers/linux/geckodriver");
-=======
-        System.setProperty("webdriver.gecko.driver", "./drivers/linux/geckodriver");
->>>>>>> ccf424a252a119cf7739ac9067518fcbdfaf6c73:src/test/java/CallbackTest.java
         WebDriverManager.firefoxdriver().setup();
     }
 
     @BeforeEach
     public void setUp() {
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        FirefoxOptions options = new FirefoxOptions();
+        options.setBinary(firefoxBinary);
+        options.setHeadless(true);
         driver = new FirefoxDriver();
     }
 
@@ -35,15 +33,11 @@ public class CallbackTest {
     @Test
     void shouldSubmitRequest() {
         driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("Сюзанна");
-        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79788586822");
-     //   List<WebElement> elements = driver.findElements(By.className("input__control"));
-     //   elements.get(0).sendKeys("Василий");
-      //  elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79788586872");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText().trim();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         assertEquals(expected, text);
 
